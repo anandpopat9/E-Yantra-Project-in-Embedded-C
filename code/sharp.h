d2b461a903027a41a4322873082c5a75266ebd8e
Anand Prashant <apopat@buffalo.edu>
2017-09-14 19:55:29 -0400

    first commit

---
+++ code/sharp.h
@@ -0,0 +1,178 @@
+/*
+ * sharp.h
+ *
+ * Created: 2/22/2015 1:05:47 AM
+ *  Author: anandpopat
+ */ 
+/********************************************************************************
+ Written by: Vinod Desai,Sachitanand Malewar NEX Robotics Pvt. Ltd.
+ Edited by: e-Yantra team
+ AVR Studio Version 6
+
+ Date: 19th October 2012
+
+
+ In this experiment ADC captures the analog sensor values and displays it on the LCD
+
+ Concepts covered:  ADC, LCD interfacing
+
+ LCD Connections:
+ 			  LCD	  Microcontroller Pins
+ 			  RS  --> PC0
+			  RW  --> PC1
+			  EN  --> PC2
+			  DB7 --> PC7
+			  DB6 --> PC6
+			  DB5 --> PC5
+			  DB4 --> PC4
+
+ ADC Connection:
+ 			  ACD CH.	PORT	Sensor
+			  0			PF0		Battery Voltage
+			  1			PF1		White line sensor 3
+			  2			PF2		White line sensor 2
+			  3			PF3		White line sensor 1
+			  4			PF4		IR Proximity analog sensor 1*****
+			  5			PF5		IR Proximity analog sensor 2*****
+			  6			PF6		IR Proximity analog sensor 3*****
+			  7			PF7		IR Proximity analog sensor 4*****
+			  8			PK0		IR Proximity analog sensor 5
+			  9			PK1		Sharp IR range sensor 1
+			  10		PK2		Sharp IR range sensor 2
+			  11		PK3		Sharp IR range sensor 3
+			  12		PK4		Sharp IR range sensor 4
+			  13		PK5		Sharp IR range sensor 5
+			  14		PK6		Servo Pod 1
+			  15		PK7		Servo Pod 2
+
+ ***** For using Analog IR proximity (1, 2, 3 and 4) sensors short the jumper J2. 
+ 	   To use JTAG via expansion slot of the microcontroller socket remove these jumpers.  
+ 
+ LCD Display interpretation:
+ ****************************************************************************
+ *BATTERY VOLTAGE	IR PROX.SENSOR 2	IR PROX.SENSOR 3	IR.PROX.SENSOR 4*
+ *LEFT WL SENSOR	CENTER WL SENSOR	RIGHT WL SENSOR		FRONT SHARP DIS *
+ ****************************************************************************
+ 
+ Note: 
+ 
+ 1. Make sure that in the configuration options following settings are 
+ 	done for proper operation of the code
+
+ 	Microcontroller: atmega2560
+ 	Frequency: 14745600s
+ 	Optimization: -O0 (For more information read section: Selecting proper optimization 
+ 					options below figure 2.22 in the Software Manual)
+
+ 2. Make sure that you copy the lcd.c file in your folder
+
+ 3. Distance calculation is for Sharp GP2D12 (10cm-80cm) IR Range sensor
+
+*********************************************************************************/
+
+/********************************************************************************
+
+   Copyright (c) 2010, NEX Robotics Pvt. Ltd.                       -*- c -*-
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are met:
+
+   * Redistributions of source code must retain the above copyright
+     notice, this list of conditions and the following disclaimer.
+
+   * Redistributions in binary form must reproduce the above copyright
+     notice, this list of conditions and the following disclaimer in
+     the documentation and/or other materials provided with the
+     distribution.
+
+   * Neither the name of the copyright holders nor the names of
+     contributors may be used to endorse or promote products derived
+     from this software without specific prior written permission.
+
+   * Source code can be used for academic purpose. 
+	 For commercial use permission form the author needs to be taken.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE. 
+
+  Software released under Creative Commence cc by-nc-sa licence.
+  For legal information refer to: 
+  http://creativecommons.org/licenses/by-nc-sa/3.0/legalcode
+
+********************************************************************************/
+#define F_CPU 14745600
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <math.h> //included to support power function
+
+
+unsigned char ADC_Conversion(unsigned char);
+unsigned char ADC_Value;
+unsigned char sharp, distance, adc_reading;
+unsigned int value;
+float BATT_Voltage, BATT_V;
+
+
+
+//ADC pin configuration
+
+//Function to Initialize PORTS
+
+//Function to Initialize ADC
+
+
+
+//This Function accepts the Channel Number and returns the corresponding Analog Value 
+
+
+
+
+// This Function prints the Analog Value Of Corresponding Channel No. at required Row
+
+
+// This Function calculates the actual distance in millimeters(mm) from the input
+// analog value of Sharp Sensor. 
+unsigned int Sharp_GP2D12_estimation(unsigned char adc_reading)
+{
+	float distance;
+	unsigned int distanceInt;
+	distance = (int)(10.00*(2799.6*(1.00/(pow(adc_reading,1.1546)))));
+	distanceInt = (int)distance;
+	if(distanceInt>800)
+	{
+		distanceInt=800;
+	}
+	return distanceInt;
+}
+
+
+
+
+//Main Function
+
+
+
+		 						//Analog Value Of Front Sharp Sensor
+
+		/*sharp = ADC_Conversion(9);						//Stores the Analog value of front sharp connected to ADC channel 11 into variable "sharp"
+		value = Sharp_GP2D12_estimation(sharp);	
+		//Stores Distance calsulated in a variable "value".
+		lcd_print(1,1,value,3); 	
+		sharp = ADC_Conversion(13);						//Stores the Analog value of front sharp connected to ADC channel 11 into variable "sharp"
+		value = Sharp_GP2D12_estimation(sharp);
+					//Stores Distance calsulated in a variable "value".
+						//Prints Value Of Distanc in MM measured by Sharp Sensor.
+	}
+}
